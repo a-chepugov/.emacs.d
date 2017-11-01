382b662c84b5bddfa64b3d1fb90a708e6247733b
a.chepugov <oleksii.chepugov@gmail.com>
2017-11-01 12:21:53 +0200

    ESC bind

--- config/global-mode/keys.el
+++ config/global-mode/keys.el
@@ -2,3 +2,6 @@
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 (global-set-key (kbd "C-c <up>")    'windmove-up)
 (global-set-key (kbd "C-c <down>")  'windmove-down)
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "C-<escape>")  'keyboard-quit)
