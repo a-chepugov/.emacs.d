644d5eb8a1505c32669540012c5573c08fd8c255
a.chepugov <oleksii.chepugov@gmail.com>
2017-11-01 18:53:22 +0200

    bind ESC

--- config/global-mode/keys.el
+++ config/global-mode/keys.el
@@ -1,7 +1,9 @@
+;; перемещение между кадрами
 (global-set-key (kbd "C-c <left>")  'windmove-left)
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 (global-set-key (kbd "C-c <up>")    'windmove-up)
 (global-set-key (kbd "C-c <down>")  'windmove-down)
 
+;; отмена действия
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (global-set-key (kbd "C-<escape>")  'keyboard-quit)
