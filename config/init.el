7ccd7312b15a30a19f3e6dda2276b3b89bf013fb
a.chepugov <a.chepugov@gmail.com>
2017-11-07 11:17:43 +0200

    setq-default + обработка ссылок на буферы

--- config/init.el
+++ config/init.el
@@ -1,6 +1,6 @@
 (load (get-relative-file-path "./init-functions"))
 
-(setq inhibit-startup-screen t)
+(setq-default inhibit-startup-screen t)
 (switch-to-buffer "*Messages*")
 
 (load (get-relative-file-path "./packages/init"))
@@ -14,15 +14,18 @@
 ;;(scroll-bar-mode   -1) ;; отключаем полосу прокрутки
 ;;(blink-cursor-mode -1) ;; курсор не мигает
 ;;(setq use-dialog-box     nil) ;; никаких графических диалогов и окон - все через минибуфер
-(setq redisplay-dont-pause t)  ;; лучшая отрисовка буфера
+(setq-default redisplay-dont-pause t)  ;; лучшая отрисовка буфера
 
-(setq visible-bell t) ;; сделать звуковый сигнал визуальным
+(setq-default visible-bell t) ;; сделать звуковый сигнал визуальным
 ;;(setq ring-bell-function 'ignore) ;; отключить звуковой сигнал
 
 ;; Disable backup/autosave files
-(setq make-backup-files        nil)
-(setq auto-save-default        nil)
-(setq auto-save-list-file-name nil)
+(setq-default make-backup-files        nil)
+(setq-default auto-save-default        nil)
+(setq-default auto-save-list-file-name nil)
+
+(setq-default find-file-existing-other-name t) ;; Поиск буферов, соответствующих также текущему файлу (ссылка)
+(setq-default find-file-visit-truename t)
 
 ;; Fringe settings
 (fringe-mode '(8 . 0)) ;; ограничитель текста только слева
@@ -30,16 +33,16 @@
 (setq-default indicate-buffer-boundaries 'left) ;; индикация только слева
 
 ;; Display file size/time in mode-line
-(setq display-time-24hr-format t) ;; 24-часовой временной формат в mode-line
+(setq-default display-time-24hr-format t) ;; 24-часовой временной формат в mode-line
 (display-time-mode             t) ;; показывать часы в mode-line
 (size-indication-mode          t) ;; размер файла в %-ах
 
 ;; Clipboard settings
-(setq x-select-enable-clipboard t) ;; Общий с ОС буфер обмена
+(setq-default x-select-enable-clipboard t) ;; Общий с ОС буфер обмена
 
 ;; Highlight search results
-(setq search-highlight        t) ;; Выделять результаты поиска
-(setq query-replace-highlight t)
+(setq-default search-highlight        t) ;; Выделять результаты поиска
+(setq-default query-replace-highlight t)
 
 (setq-default truncate-lines t) ;; Усекать отображение строк, выходящих за размер окна
 
