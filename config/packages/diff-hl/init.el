37e806a2bbe7517205e289ece06bd3bc71f0ac49
a.chepugov <a.chepugov@gmail.com>
2017-11-09 19:38:12 +0200

    fix. diff-hl (ошибка переполнения стека при revert-buffer)

--- config/packages/diff-hl/init.el
+++ config/packages/diff-hl/init.el
@@ -1,3 +1,9 @@
-(custom-set-variables '(global-diff-hl-mode t))
+(add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
+(add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
+
+(add-hook 'js3-mode-hook 'turn-on-diff-hl-mode)
+(add-hook 'html-mode-hook 'turn-on-diff-hl-mode)
+(add-hook 'smarty-mode-hook 'turn-on-diff-hl-mode)
+
 (custom-set-variables '(diff-hl-margin-mode t))
 (custom-set-variables '(diff-hl-flydiff-mode t))
