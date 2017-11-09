c63843317c70ba5790658191fdfbfa1f085400fb
a.chepugov <a.chepugov@gmail.com>
2017-11-09 19:38:32 +0200

    chore

--- config/global-mode/keys.el
+++ config/global-mode/keys.el
@@ -5,13 +5,12 @@
 (global-set-key (kbd "C-c <down>")  'windmove-down)
 
 ;; отмена действия
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-(global-set-key (kbd "C-<escape>")  'keyboard-quit)
+(global-set-key (kbd "C-<escape>") 'keyboard-escape-quit)
 
-(global-set-key (kbd "C-c C-i")  'invert-face+) ;; инвертирование цветовой схемы
+(global-set-key (kbd "C-c C-i") 'invert-face+) ;; инвертирование цветовой схемы
 
-(global-set-key (kbd "C-<f4>")  'kill-buffer) ;; уничтожить буфер
+(global-set-key (kbd "C-<f4>") 'kill-buffer) ;; уничтожить буфер
 
 ;; дублировать строку или область
-(global-set-key (kbd "C-c d")  'duplicate-current-line-or-region)
-(global-set-key (kbd "C-c C-d")  'duplicate-current-line-or-region)
+(global-set-key (kbd "C-c d") 'duplicate-current-line-or-region)
+(global-set-key (kbd "C-c C-d") 'duplicate-current-line-or-region)
